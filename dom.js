27a4ede33893fb6ec401d31c47fc19574b83917a
Saksham Rai <raisaksham2001@gmail.com>
2023-08-01 17:23:29 -0700

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,5 +1,3 @@
-/* dom.js */
-
 function init() {
     let element = document.getElementById('walkBtn');
     element.addEventListener('click', function () {
@@ -34,6 +32,29 @@ function init() {
     element = document.getElementById('removeBtn');
     element.addEventListener('click', function () {
         remove();
+    });
+
+    element = document.getElementById('safeDeleteBtn');
+    element.addEventListener('click', function () {
+        safeDelete();
+    });
+
+    element = document.getElementById('deleteBySelectorBtn');
+    element.addEventListener('click', function () {
+        const selectorInput = document.getElementById('deleteSelector').value.trim();
+        if (selectorInput) {
+            deleteBySelector(selectorInput);
+        }
+    });
+
+    element = document.getElementById('basicCloneBtn');
+    element.addEventListener('click', function () {
+        basicClone();
+    });
+
+    element = document.getElementById('advancedCloneBtn');
+    element.addEventListener('click', function () {
+        advancedClone();
     });
 }
 
@@ -211,6 +232,59 @@ function ElementWithTimestamp(tagname) {
 
 function remove() {
     document.body.removeChild(document.body.lastChild);
+}
+
+function safeDelete() {
+  
+    const controlsSection = document.getElementById('controls');
+
+  
+    const elementsToDelete = [];
+    let currentElement = document.body.lastChild;
+    while (currentElement !== controlsSection) {
+        elementsToDelete.push(currentElement);
+        currentElement = currentElement.previousSibling;
+    }
+
+    
+    elementsToDelete.forEach((element) => {
+        if (element.nodeType === Node.ELEMENT_NODE) {
+            
+            if (element.tagName.toLowerCase() === 'button' || element === controlsSection) {
+                return;
+            }
+            element.parentNode.removeChild(element);
+        }
+    });
+}
+
+function deleteBySelector(selector) {
+    const elementsToDelete = document.querySelectorAll(selector);
+    elementsToDelete.forEach((element) => {
+        if (element.parentNode) {
+            element.parentNode.removeChild(element);
+        }
+    });
+}
+
+function basicClone() {
+    const p1 = document.getElementById('p1');
+    const clonedP = p1.cloneNode(true);
+    clonedP.id = ''; 
+    document.body.appendChild(clonedP); 
+}
+
+function advancedClone() {
+    const cardTemplate = document.getElementById('cardTemplate');
+    const clonedCard = cardTemplate.content.cloneNode(true);
+
+    const titleElement = clonedCard.querySelector('.card-title');
+    const textElement = clonedCard.querySelector('.card-text');
+    const randomNumber = Math.floor(Math.random() * 1000);
+    titleElement.textContent = `Card Title ${randomNumber}`;
+    textElement.textContent = `Card description for card ${randomNumber}.`;
+
+    document.body.appendChild(clonedCard);
 }
 
 window.addEventListener('DOMContentLoaded', init);
